d4dbe4fb9655c6a5fced85368bfec879095bde6f
ImKartikey27 <kartikey.22b1541008@abes.ac.in>
2024-11-11 01:00:58 +0530

    53.Maximum Subarray Uploaded

---
+++ maximumsubarray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maximumsubarraysum(vector<int>& nums){
+  int size=nums.size();
+  int sum=0;
+  int maxi=nums[0];
+  for(int i=0;i<size;i++){
+    sum=sum+nums[i];
+    maxi=max(maxi,sum);
+    if(sum<0){
+      sum=0;
+    }
+  }
+  return maxi;
+}
+int main(){
+  vector<int>nums={-2,1,-3,4,-1,2,1,-5,4};
+  int ans=maximumsubarraysum(nums);
+  cout<<ans<<endl;
+  return 0;
+}
